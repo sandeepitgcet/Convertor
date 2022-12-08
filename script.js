ec3e829ae2a6b95074548371cee4cfe945911756
sandeepitgcet <sandeepitgcet@gmail.com>
2022-12-09 03:32:38 +0530

    updated html css

---
+++ script.js
@@ -0,0 +1,58 @@
+let from=document.getElementById("from");
+let to=document.getElementById("to");
+let value=document.getElementById("value");
+let result=document.getElementById("result");
+
+document.getElementById("swap").addEventListener("click",function(){
+    let temp=from.value;
+    from.value=to.value;
+    to.value=temp;
+
+    temp=value.value;
+    value.value=result.value;
+    result.value=temp;
+
+    
+});
+document.getElementById("from").addEventListener("onchange",function(){
+
+});
+document.getElementById("convert").addEventListener("click",function(){
+    
+    if(isValid()){
+        AnyToAny();
+    }else{
+        alert("Enter valid Input Value:");
+    }
+});
+
+function isValid(){
+    if(value.value=="") return false;
+    else if(from.value=="binary"){
+        return !isNaN(parseInt(value.value,2));
+    }
+    return true;
+}
+
+
+function AnyToAny(){
+    let base=10;
+    let t=from.value;
+    if(t=="binary") base=2;
+    else if(t=="octal") base=8;
+    else if(t=="hexadecimal") base=16;
+    else base=10;
+
+    let radix=10;
+    t=to.value;
+    if(t=="binary"){
+        radix=2;
+    }else if(t=="octal"){
+        radix=8;
+    }else if(t=="hexadecimal"){
+        radix=16;
+    }
+    let decimal = parseInt(value.value, base).toString(radix);
+    result.value = decimal;
+}
+
